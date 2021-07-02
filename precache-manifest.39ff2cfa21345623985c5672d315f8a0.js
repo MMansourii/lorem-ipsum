25d065e9e40a603a88950817be8b4bb3c3b82f7b
Mohammad <57011800+Mammad10@users.noreply.github.com>
2021-07-02 08:58:38 +0300

    Updates

---
+++ precache-manifest.39ff2cfa21345623985c5672d315f8a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f77dc0c4011a7f8943dcfb4dff7a587",
+    "url": "/github.io/lorem-ipsum/index.html"
+  },
+  {
+    "revision": "c6361f16b318b6b5363a",
+    "url": "/github.io/lorem-ipsum/static/css/main.9d994fcb.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/github.io/lorem-ipsum/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/lorem-ipsum/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6361f16b318b6b5363a",
+    "url": "/github.io/lorem-ipsum/static/js/main.49c361b1.chunk.js"
+  },
+  {
+    "revision": "8959b0509ee3061ee158",
+    "url": "/github.io/lorem-ipsum/static/js/runtime-main.398f2ab1.js"
+  }
+]);
